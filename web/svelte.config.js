7f658f3ec1a414275eaf5f3b595be9b92bcfa4d3
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2026-02-22 09:31:15 -0500

    test(web): verify all phase 3 tests pass

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,12 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: '404.html',
+      pages: "build",
+      assets: "build",
+      fallback: "404.html",
       precompress: false,
       strict: true,
     }),
